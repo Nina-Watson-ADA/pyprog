b0bab2095dc997e00eec4f653aea314569448ea6
Nina-Watson-ADA <nina.watson@ada.ac.uk>
2025-04-30 11:22:46 +0000

    adding the multiplicationq

--- multargument.py
+++ multargument.py
@@ -1,8 +1,8 @@
 import numpy as np
 arr = [12, 3, 4, 15]
 arr2 = np.multiply(arr, 2)
-print (arr)
-print(arr2)
+print ("origin array",arr)
+#print(arr2)
 #print each on a different line
 
 for x in arr2:
